f4ee17fa9ae8dde2e71c187b8cc2322d1e30a5d2
timur1213 <timur1213@mail.ru>
2024-02-15 15:58:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<String>();
+        while (true)
+        {
+            String s = reader.readLine();
+            if (s.equals("end")) {
+                break;
+            }
+            else {
+                list.add(s);
+            }
+        }
+        for (int a = 0; a < list.size(); a++)
+            System.out.println(list.get(a));
+
+    }
+}
