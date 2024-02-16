ed1072494365e15283978418975b88cbf3ece3af
timur1213 <timur1213@mail.ru>
2024-02-16 12:01:07 +0200

    first

--- src/cats.java
+++ src/cats.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class cats {
+
+import java.util.HashMap;
+import java.util.Map;
+public class cats
+{
+    public static void main(String[] args) throws Exception
+    {
+        String[] cats = new java.lang.String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
+        String[] color = new java.lang.String[]{"белый", "черный","пятнистый","красный","синый","пепельный","коричневый","голубой","рыжий","полосатый"};
+        Map<String, Cat> map = addCatsToMap(cats);
+
+        for (Map.Entry<String, Cat> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+
+    public static Map<String, Cat> addCatsToMap(String[] cats)
+    {
+        Map<String, Cat> addCats = new HashMap<String, Cat>();
+        for(String s: cats){
+            addCats.put(s, new Cat(s));
+        }
+        return addCats;
+    }
+
+    public static class Cat
+    {
+        String name;
+
+        public Cat(String name)
+        {
+            this.name = name;
+        }
+        String color;
+        public void Color(String color)
+        {
+            this.color = color;
+        }
+    }
 }
